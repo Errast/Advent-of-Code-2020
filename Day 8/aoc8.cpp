f212cc87375211d9c140d84e120d2a6c03ec7fb8
Errast <hurricanezombie47@gmail.com>
2020-12-08 09:58:59 -0500

    Day 8 part 2 - so this question didn't like my polymorphism

--- Day 8/aoc8.cpp
+++ Day 8/aoc8.cpp
@@ -5,65 +5,76 @@
 #include <algorithm>
 #include "../Global/my_utils.cpp"
 
+enum class operations {
+    op_nop,
+    op_acc,
+    op_jmp
+};
+
 struct operation {
     int arg{ };
-    operation(int arg) : arg{ arg } { }
+    operations op;
+    operation(operations op, int arg) : arg{ arg }, op{ op } { }
 
-    virtual void execute(int& instrPos, int& acc) = 0;
-};
-
-struct op_nop : operation {
-    op_nop(int arg) : operation( arg ) { }
-
-    void execute(int& instrPos, int& acc) override {
-        instrPos++;
+    void execute(int& instrPos, int& acc) {
+        switch(op) {
+            case operations::op_nop:
+                instrPos++;
+                break;
+            case operations::op_acc:
+                acc += arg;
+                instrPos++;
+                break;
+            case operations::op_jmp:
+                instrPos += arg;
+        }
     }
 };
 
-struct op_acc : operation {
-    op_acc(int amount) : operation( arg ) { }
-    
-    void execute(int& instrPos, int& acc) override {
-        acc += arg;
-        instrPos++;
-    }
-};
-
-struct op_jmp : operation {
-    op_jmp(int offset) : operation( arg ) { }
-
-    void execute(int& instrPos, int& acc) override {
-        instrPos += arg;
-    }
-};
-
-operation* parseOp(std::string str) {
-    operation* op{ };
+operation parseOp(std::string str) {
     size_t argSeparator{ str.find(' ') };
     std::string opName{ str.substr(0, argSeparator) };
     int arg{ std::stoi(str.substr(argSeparator + 1)) };
-    if(opName == "nop")
-        op = new op_nop{ arg };
-    else if(opName == "acc")
-        op = new op_acc{ arg };
-    else if(opName == "jmp")
-        op = new op_jmp{ arg };
-
-    return op;
+    if(opName == "acc")
+        return operation{ operations::op_acc, arg };
+    if(opName == "jmp")
+        return operation{ operations::op_jmp, arg };
+    
+    return operation{ operations::op_nop, arg };
 }
 
-bool infiniteLoop(const std::vector<operation*>& program, int& instrPos, int& acc) {
-    const int programLength{ program.size() };
+bool infiniteLoop(std::vector<operation>& program, int& instrPos, int& acc) {
+    const size_t programLength{ program.size() };
     // because std::vector<bool> is a thing
     std::vector<int> visited( programLength );
     while(instrPos < programLength) {
         if(visited[instrPos])
-            return false;
+            return true;
         visited[instrPos] = true;
-        program[instrPos]->execute(instrPos, acc);
+        program[instrPos].execute(instrPos, acc);
     }
 
-    return true;
+    return false;
+}
+
+void fixInfiniteLoop(std::vector<operation>& program, int& instrPos, int& acc) {
+    for(auto& instr : program) {
+        if(instr.op != operations::op_nop && instr.op != operations::op_jmp) {
+            continue;
+        }
+
+        bool isJmp{ instr.op == operations::op_jmp };
+        // if(isJmp && instr.arg > 0) {
+        //     continue;
+        // }
+
+        instr.op = isJmp ? operations::op_nop : operations::op_jmp;
+        instrPos = acc = 0;
+        if(!infiniteLoop(program, instrPos, acc)) {
+            return;
+        }
+        instr.op = instr.op = isJmp ? operations::op_jmp : operations::op_nop;
+    }
 }
 
 int main() {
@@ -73,12 +84,16 @@ int main() {
     std::istream_iterator<std::string> end{ };
     std::vector<std::string> strings{ start, end };
 
-    std::vector<operation*> program{ };
+    std::vector<operation> program{ };
     std::transform(strings.begin(), strings.end(), std::back_inserter(program), parseOp);
 
     // part 1
     int instrPos{ 0 };
     int acc{ 0 };
     infiniteLoop(program, instrPos, acc);
-    std::cout << acc;
+    std::cout << acc << '\n';
+
+    // part 2
+    fixInfiniteLoop(program, instrPos, acc);
+    std::cout << acc << '\n';
 }
