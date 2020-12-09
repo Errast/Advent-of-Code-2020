ca1939bddadf453c803f733bc893aa30af7014c0
Errast <hurricanezombie47@gmail.com>
2020-12-09 01:30:50 -0500

    Day 9 part 2

--- Day 9/aoc9.cpp
+++ Day 9/aoc9.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <string>
 #include <string_view>
+#include <algorithm>
+
+template<typename T>
+using vec_iter_const = typename std::vector<T>::const_iterator;
 
 constexpr std::string_view file_name{ "9 - input.txt" };
 constexpr int previousNums() {
@@ -30,6 +34,21 @@ bool isValid(int index, const std::vector<int>& nums) {
     return false;
 }
 
+template<typename T>
+std::pair<vec_iter_const<T>, vec_iter_const<T>> findRangeSumTo(vec_iter_const<T> start, vec_iter_const<T> end, T value) {
+    for(auto rangeStart{ start }; rangeStart != end; ++rangeStart) {
+        auto rangeEnd{ rangeStart + 2 };
+        T total { rangeStart[0] + rangeStart[1] };
+        while(total < value && rangeEnd != end) {
+            total += *rangeEnd;
+            ++rangeEnd;
+        }
+        if(total == value) {
+            return {rangeStart, rangeEnd - 1};
+        }
+    }
+}
+
 int main() {
     std::ifstream input{ std::string{ file_name } };
     const std::istream_iterator<int> start{ input };
@@ -45,4 +64,10 @@ int main() {
         }
     }
     std::cout << invalidNum << '\n';
+
+    // part 2
+    auto result{ findRangeSumTo(nums.begin(), nums.end(), invalidNum) };
+    int min{ *std::min_element(result.first, result.second) };
+    int max{ *std::max_element(result.first, result.second) };
+    std::cout << min + max << '\n';
 }
