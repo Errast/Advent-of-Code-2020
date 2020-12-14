f391ea1e7f0e252f4e861eee30d907f9632cfaee
Errast <hurricanezombie47@gmail.com>
2020-12-13 20:42:39 -0500

    Day 10 part 2 or: why you shouldn't code at 1:30

--- Day 10/aoc10.cpp
+++ Day 10/aoc10.cpp
@@ -2,13 +2,22 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 #include "../Global/my_utils.cpp"
 
 struct diff_counts {
-    int zero{ };
     int one{ };
     int two{ };
     int three{ };
+    // Includes all "extr" adapters with this joltage. The real number of adapters with this joltage is one plus this
+    int zero{ };
+
+    inline bool isAllZero() const {
+        return zero == 0
+               && one == 0
+               && two == 0
+               && three == 0;
+    }
 };
 
 std::vector<int> makeDiffs(const std::vector<int>& adapters) {
@@ -25,8 +34,7 @@ std::vector<int> makeDiffs(const std::vector<int>& adapters) {
     return diffs;
 }
 
-diff_counts countDiffs(const std::vector<int>& adapters) {
-    std::vector<int> diffs( makeDiffs(adapters) );
+diff_counts countDiffs(const std::vector<int>& diffs) {
     diff_counts count{ };
 
     for(auto elem : diffs) {
@@ -41,9 +49,73 @@ diff_counts countDiffs(const std::vector<int>& adapters) {
     return count;
 }
 
-int main() {
-    auto adapters{ getData<int>("10 - input.txt") };
+std::map<int, diff_counts> makeDiffTree(const std::vector<int>& adapters) {
+    std::map<int, diff_counts> map{ };
+    std::vector<int> sorted{ 0 };
+    sorted.insert(sorted.end(), adapters.begin(), adapters.end());
+    std::sort(sorted.begin(), sorted.end());
+
+    for(int i{ 0 }; i < sorted.size(); i++) {
+        diff_counts count{ };
+        for(int j{ i + 1 }, diff{ -1 };
+            j < sorted.size() && (diff = sorted[j] - sorted[i]) <= 3;
+            j++) {
+            switch(diff) {
+                case 0:  ++count.zero; ++i;  break;
+                case 1:  ++count.one;   break;
+                case 2:  ++count.two;   break;
+                case 3:  ++count.three; break;
+            }
+        }
+        // if(!count.isAllZero()) {
+        map[sorted[i]] = count;
+        // }
+    }
     
-    auto count{ countDiffs(adapters) };
+    return map;
+}
+
+// This deals with multiple adapters with the same joltage
+// Actually, I know the number gets bigger if you add a duplicate,
+// but I haven't bothered to figure out if it's the right number
+int64_t countPathsTo(int root,
+                 int end,
+                 const std::map<int, diff_counts>& map,
+                 std::map<std::pair<int, int>, int64_t>& cached = std::map<std::pair<int, int>, int64_t>{ }) {
+
+    if(mine::hasKey({ end, root }, cached)) {
+        return cached[{ end, root }];
+    }
+
+    int64_t count{ 0 };
+    if(root == end) {
+        count = 1;
+    } else {
+        auto diffs{ map.at(root) };
+        if(diffs.one != 0) {
+            count += mine::factorial(diffs.one) * countPathsTo(root + 1, end, map, cached);
+        }
+        if(diffs.two != 0) {
+            count += mine::factorial(diffs.two) * countPathsTo(root + 2, end, map, cached);
+        }
+        if(diffs.three != 0) {
+            count += mine::factorial(diffs.three) * countPathsTo(root + 3, end, map, cached);
+        }
+    }
+
+    cached[{end, root}] = count;
+    return count;
+}
+
+int main() {
+    auto adapters{ mine::getData<int>("10 - input.txt") };
+    
+    // part 1
+    auto diffs{ makeDiffs(adapters) };
+    auto count{ countDiffs(diffs) };
     std::cout << count.one * count.three << '\n';
+
+    // part 2
+    auto diffTree{ makeDiffTree(adapters) };
+    std::cout << countPathsTo(0, *std::max_element(adapters.begin(), adapters.end()), diffTree) << '\n';
 }
--- Global/my_utils.cpp
+++ Global/my_utils.cpp
@@ -2,11 +2,48 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <stdexcept>
+#include <map>
 
-template<typename T>
-std::vector<T> getData(const char* fileName) {
-    std::ifstream input{ fileName };
-    const std::istream_iterator<int> start{ input };
-    const std::istream_iterator<int> end{ };
-    return std::vector<T>{ start, end };
+namespace mine {
+    template<typename T>
+    std::vector<T> getData(const char* fileName) {
+        std::ifstream input{ fileName };
+        const std::istream_iterator<int> start{ input };
+        const std::istream_iterator<int> end{ };
+        return std::vector<T>{ start, end };
+    }
+
+    constexpr int64_t factorial(int64_t num) {
+        if(num < 0) {
+            throw std::invalid_argument{ "int factorial(int num): num cannot be negative" };
+        }
+
+        int total { 1 };
+        for(int i = 2; i <= num; i++) {
+            total *= num;
+        }
+        return total;
+    }
+
+    template<typename T, typename S>
+    inline bool hasKey(const T& key, const std::map<T, S>& map) {
+        return map.find(key) != map.end();
+    }
+
+    template<typename T>
+    int totalDepth(T value, 
+                   const std::map<T, std::vector<std::pair<int, T>>>& map, 
+                   std::map<T, int>& cache = std::map<T, int>{ }) {
+        if(mine::hasKey(value, cache)) {
+            return cache[value];
+        }
+    
+        int count{ 1 };
+        for(auto elem : map.at(value)) {
+            count += elem.first * totalDepth(elem.second, map, cache);
+        }
+        cache[value] = count;
+        return count;
+    }
 }
